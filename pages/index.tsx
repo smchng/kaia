b16e3ca103144cb20ea890aa1ba177c6b95a0cd8
Samantha <scminim100@gmail.com>
2024-04-23 19:52:00 +0200

    new page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,70 @@
 import "@/styles/global.css";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
+  const [video, setVideo] = useState<HTMLVideoElement | null>(null);
+  const [showMore, setShowMore] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  function playVideo() {
+    if (video) {
+      video.muted = false;
+      video.play();
+      setShowMore(true);
+    }
+  }
   return (
     <main>
       <div className="absolute inset-0 flex justify-center items-center">
-        <video className="" autoPlay loop>
-          <source src="/vids/Kaia_1.mp4" type="video/mp4" />
+        <video
+          ref={(el) => setVideo(el)}
+          className="md:h-screen md:w-auto h-auto w-screen"
+          loop
+        >
+          <source src="/vids/kaia.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
+      <div
+        className={`${
+          isFlipped ? "scale-x-[-1]" : ""
+        } absolute flex justify-center items-end w-screen h-screen `}
+      >
+        <Image
+          className=" w-[20vw] h-auto"
+          src="/imgs/hamster.png"
+          width={100}
+          height={100}
+          alt="hamster"
+        />
+      </div>
+
       <div className="absolute inset-0 flex justify-center items-center">
-        <div className="hover:bg-yellow bg-red cursor-pointer border rounded-xl border-[1.5px] px-5 pb-1 transition-colors">
-          <p className="text-base">
-            <strong>more</strong>
-          </p>
-        </div>
+        {showMore ? (
+          <Link
+            onMouseEnter={() => setIsFlipped(true)}
+            onMouseLeave={() => setIsFlipped(false)}
+            href="/kaia"
+            className="hover:bg-yellow bg-red cursor-pointer border rounded-xl border-[1.5px] px-5 pb-1 transition-colors"
+          >
+            <p className="text-xs">
+              <strong>more</strong>
+            </p>
+          </Link>
+        ) : (
+          <button
+            onMouseEnter={() => setIsFlipped(true)}
+            onMouseLeave={() => setIsFlipped(false)}
+            onClick={playVideo}
+            className="hover:bg-yellow bg-red cursor-pointer border rounded-xl border-[1.5px] px-5 pb-1 transition-colors"
+          >
+            <p className="text-xs">
+              <strong>secret!</strong>
+            </p>
+          </button>
+        )}
       </div>
     </main>
   );
---
+++ pages/kaia.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+export default function Kaia() {
+  return (
+    <div>
+      <div className="absolute flex h-screen w-full justify-center items-center">
+        <video
+          className="md:h-screen md:w-auto h-auto w-screen "
+          autoPlay
+          controls
+          loop
+        >
+          <source src="/vids/kaia2.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div
+        className={` absolute flex justify-center items-end w-screen h-screen `}
+      >
+        <Image
+          className=" w-[20vw] h-auto"
+          src="/imgs/cake.svg"
+          width={100}
+          height={100}
+          alt="hamster"
+        />
+      </div>
+      {/* <div className="absolute flex h-auto w-screen justify-center items-center z-10">
+        <video className="" autoPlay muted loop>
+          <source src="/vids/flowers.mov" type="video/quicktime" />
+          Your browser does not support the video tag.
+        </video>
+      </div> */}
+    </div>
+  );
+}
